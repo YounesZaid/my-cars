cfa41ae880eda1adca04e16ce439613d02c0d943
Zaid Younes <zaidyounes94@gmail.com>
2018-04-02 22:23:46 +0100

    Added side bar content

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './index.css';
 
 const Sidebar = ({ toggleDrawer }) => ([
-  <aside id="sidebar" key={1}></aside>,
+  <aside id="sidebar" key={1}>
+    <a href="#logo" className="logo">LOGO</a>
+    <nav className="nav-list">
+      <ul>
+        <li><a href="#trips" className="active"> Trips </a></li>
+        <li><a href="#drivers"> Drivers </a></li>
+        <li><a href="#cars"> Cars </a></li>
+        <li><a href="#cards"> Cards </a></li>
+      </ul>
+    </nav>
+  </aside>,
   <div onClick={toggleDrawer} key={0} id="sidebar-backdrop" />,
 ])
 
