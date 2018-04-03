087406b5860c1e6117fa1b1e17d2c1ec5ab4ae2e
Zaid Younes <zaidyounes94@gmail.com>
2018-04-03 16:30:24 +0100

    Added Screens

---
+++ src/Screens/Cards/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+export default class CardsScreen extends Comment {
+  render() {
+    return (
+      <div className="screen-wrapper cards-screen">
+        <header>
+          <h3>Cards</h3>
+          <a href="#add-content">ADD NEW CARDS <i className="zmdi zmdi-plus-square"></i></a>
+        </header>
+        <section>
+          
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/Screens/Drivers/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+export default class DriversScreen extends Component {
+  render() {
+    return (
+      <div className="screen-wrapper driver-screen">
+        <header>
+          <h3>Drivers</h3>
+          <a href="#add-content">ADD NEW DRIVER <i className="zmdi zmdi-plus-square"></i></a>
+        </header>
+        <section>
+          
+        </section>
+      </div>
+    )
+  }
+}
