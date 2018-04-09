bd23f82551235c9f69ffa71e978bf5e8f6d4c108
Younes Zaid <zaidyounes94@gmail.com>
2018-04-09 11:57:06 +0100

    Edited TripsList and TripDetails style

--- src/Screens/Trips/TripItem.js
+++ src/Screens/Trips/TripItem.js
@@ -2,22 +2,29 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 const TripItem = ({ tripId, carType, driverName, isActive, history }) => (
-  <div className="trip-item" onClick={e => {
+   <div className="trip-item" onClick={e => {
       history.push(`/trips/${tripId}`)
-  }}>
-    <p href="#trip-item" className="trip-content">
-      <span>{carType} / {driverName}</span> 
-    </p> 
-    <span className="isActive">
-      {isActive && <b>ACTIVE</b>}
-    </span>
-  </div>
+   }}>
+      {/* <p href="#trip-item" className="trip-content">
+         <span>{carType} / {driverName}</span>
+      </p>
+      <span className="isActive">
+         {isActive && <b>ACTIVE</b>}
+      </span> */}
+      <ul>
+         <li className="li-dirveName"><i className="zmdi zmdi-account-circle item-icon"></i> {driverName}</li>
+         <li><i className="zmdi zmdi-car item-icon"></i> {carType} </li>
+      </ul>
+      <span className="isActive">
+         {isActive && <b>ACTIVE</b>}
+      </span>
+   </div>
 )
 
 TripItem.defaulProps = {
-  carType: '',
-  driverName: '',
-  isActive: false
+   carType: '',
+   driverName: '',
+   isActive: false
 }
 
 export default withRouter(TripItem);
