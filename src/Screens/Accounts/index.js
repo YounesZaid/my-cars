d9aad4824552a5c9eff7061c38b12605ab375290
Younes Zaid <zaidyounes94@gmail.com>
2018-06-06 13:37:33 +0000

    Edited ui

--- src/Screens/Accounts/index.js
+++ src/Screens/Accounts/index.js
@@ -1,10 +1,86 @@
 import React, { Component } from 'react';
+import { Button, Intent } from '@blueprintjs/core';
+
 import './index.css';
 
-const AccountScreen = () => (
-  <div className="screen-wrapper accounts-screen">
-    <h1>Profile</h1>
-  </div>
-)
+class AccountScreen extends Component {
+
+  state = {
+    large: true,
+    minimal: true,
+    fill: true
+  }
+
+  render() {
+    const { large, minimal, fill } = this.state;
+    return (
+      <div className="screen-wrapper accounts-screen">
+        <header>
+          <h3>Accounts</h3>
+        </header>
+        <section>
+          <div className="account-header pt-card pt-elevation-2">
+            <i className="zmdi zmdi-account-add zmdi-hc-3x"></i>
+            <h5 className="page-title">Welcome, In this page youn can create users accounts to sign in to your mobile app</h5>
+          </div>
+          <section className="account-form-container">
+            <aside className="account-form">
+              <div className="pt-form-group pt-intent-primary">
+                <label className="pt-label" htmlFor="fullName">
+                  User Full Name
+              </label>
+                <div className="pt-input-group pt-large pt-intent-primary">
+                  <span className="pt-icon pt-icon-person"></span>
+                  <input id="fullName" className="pt-input " type="text" placeholder="Full Name" dir="auto" />
+                </div>
+              </div>
+              <div className="pt-form-group pt-intent-primary">
+                <label className="pt-label" htmlFor="email">
+                  Enter Email
+              </label>
+                <div className="pt-form-content">
+                  <div className="pt-input-group pt-large pt-intent-primary">
+                    <span className="pt-icon pt-icon-envelope"></span>
+                    <input id="email" className="pt-input " type="email" placeholder="Email address" dir="auto" />
+                  </div>
+                  <div className="pt-form-helper-text">Please enter a valid address</div>
+                </div>
+              </div>
+              <div className="pt-form-group pt-intent-primary">
+                <label className="pt-label" htmlFor="password">
+                  Enter Password
+              </label>
+                <div className="pt-form-content">
+                  <div className="pt-input-group pt-large pt-intent-primary">
+                    <span className="pt-icon pt-icon-lock"></span>
+                    <input id="password" className="pt-input " type="password" placeholder="Password" dir="auto" />
+                  </div>
+                  <div className="pt-form-helper-text">Please 6 characters minumum</div>
+                </div>
+              </div>
+              <button type="button" className="pt-button pt-minimal pt-large pt-icon-account" onClick={(e) => {
+                alert('creted');
+              }}>CREATE ACCOUNT</button>
+              {/* <Button
+                text="CREATE ACCOUNT"
+                icon="add"
+                disabled={true}
+                minimimal={"true"}
+                large="true"
+                intent={Intent.PRIMARY}
+                onClick={() => {
+                  alert('Created');
+                }}
+              /> */}
+            </aside>
+            <aside className="account-submit">
+              <h6>render list of users </h6>
+            </aside>
+          </section>
+        </section>
+      </div>
+    )
+  }
+}
 
 export default AccountScreen;
